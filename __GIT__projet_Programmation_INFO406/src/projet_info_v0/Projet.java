fe9e37efce695a84abb93f3571795e1f9b610bfe
Joachim-Chapard <60465250+Joachim-Chapard@users.noreply.github.com>
2020-03-26 12:11:52 +0100

    V0 source (#2)
    * Commit initial pour tester l'intégration Git dans Eclipse
    
    * Supression de fichiers inutiles à présent.
    Etape d'un guide, objectif incertain, utilité du fichier inconnu.
    Modification d'un fichier pour comprendre Git sur eclipse.
    Revert "Modification d'un fichier pour comprendre Git sur eclipse",
    This reverts commit e148c12f7a2163b8cc67b2183b7c5900ec910bd2.

---
+++ __GIT__projet_Programmation_INFO406/src/projet_info_v0/Projet.java
@@ -0,0 +1,68 @@
+package projet_info_v0;
+
+import java.util.ArrayList;
+
+public class Projet {
+	
+	private int num_proj;
+	private String nom_proj;
+	private Employee chef_de_projet;
+	private ArrayList<Personne> personnels;
+	private ArrayList<Salle> salles;
+	protected ArrayList<Ressource> ressources;
+	
+	
+	
+	public Projet(int i, String string) {
+		this.num_proj=i;
+		this.nom_proj=string;
+		this.ressources=new ArrayList<Ressource>();
+	}
+	
+	/**
+	 * @return the num_proj
+	 */
+	public int getNum_proj() {
+		return num_proj;
+	}
+	/**
+	 * @param num_proj the num_proj to set
+	 */
+	public void setNum_proj(int num_proj) {
+		this.num_proj = num_proj;
+	}
+	/**
+	 * @return the nom_proj
+	 */
+	public String getNom_proj() {
+		return nom_proj;
+	}
+	/**
+	 * @param nom_proj the nom_proj to set
+	 */
+	public void setNom_proj(String nom_proj) {
+		this.nom_proj = nom_proj;
+	}
+	/**
+	 * @return the chef_de_projet
+	 */
+	public Employee getChef_de_projet() {
+		return chef_de_projet;
+	}
+	/**
+	 * @param chef_de_projet the chef_de_projet to set
+	 */
+	public void setChef_de_projet(Employee chef_de_projet) {
+		this.chef_de_projet = chef_de_projet;
+	}
+	/**
+	 * @return the ressources
+	 */
+	
+	
+	
+	public String toString() {
+		return this.nom_proj;
+	}
+	
+}
