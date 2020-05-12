f19d6424c5fff08ccc161d801a7b0cf090833ce2
Cadrelle <59994136+Cadrelle@users.noreply.github.com>
2020-05-12 11:13:38 +0200

    fin-mise-à-jour

---
+++ Projet/src/projetjava/Ressources.java
@@ -0,0 +1,66 @@
+package projetjava;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+
+public class Ressources extends JFrame implements ActionListener{
+	
+	final static int HAUTEUR = 400 ;
+	final static int LARGEUR = 400 ;
+	
+	public Ressources() {
+		
+		setTitle("Ressources") ;
+		setSize(LARGEUR,HAUTEUR) ;
+		this.setLayout(new BorderLayout()) ;
+		this.setLayout(new FlowLayout()) ;
+		this.setBackground(Color.PINK);  
+		
+		JButton employés = new JButton ("EMPLOYES") ;
+		JButton salle = new JButton ("SALLE") ;
+		JButton ordinateur = new JButton ("ORDINATEUR") ;
+		this.add(employés , BorderLayout.WEST) ; 	
+		this.add(salle , BorderLayout.CENTER) ; 	
+		this.add(ordinateur , BorderLayout.EAST) ; 
+		
+		employés.setPreferredSize(new Dimension(100,100));	
+		salle.setPreferredSize(new Dimension(100,100));	
+		ordinateur.setPreferredSize(new Dimension(110,100));
+		
+		employés.setBackground(Color.cyan) ;
+		salle.setBackground(Color.cyan) ;
+		ordinateur.setBackground(Color.cyan) ;
+		
+		/*employés.setBounds(200,150,300,200); */
+		
+		employés.addActionListener(this) ;
+		salle.addActionListener(this) ;
+		ordinateur.addActionListener(this) ;
+		
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setVisible(true) ;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		        new Employés() ;
+	}
+	
+	public void actionPerformed1(ActionEvent arg1) {
+		        new Salle() ;
+	}
+	
+	public void actionPerformed2(ActionEvent arg2) {
+		        new Ordinateur() ;
+	}
+	   
+}
---
+++ Projet/src/projetjava/Salle.java
@@ -0,0 +1,77 @@
+package projetjava;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class Salle extends JFrame implements ActionListener{
+	private JComboBox  salle ;
+	
+	
+	final static int HAUTEUR = 300 ;
+	final static int LARGEUR = 300 ;
+	
+	public Salle() {
+		setTitle("Ressources") ;
+		setSize(LARGEUR,HAUTEUR) ;
+		this.setBackground(Color.PINK);
+		this.setLayout(new FlowLayout() );
+		JButton back = new JButton ("BACK") ;
+		this.add(back , BorderLayout.SOUTH) ;
+		back.addActionListener(this);
+		add(listedéroulante() , BorderLayout.CENTER);
+
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setVisible(true) ;
+	}
+	
+	public JPanel listedéroulante() {
+		JPanel panel = new JPanel() ;
+		
+		
+		
+		
+		
+		Object[] elements2 = new Object[] {"salle","n° salle" , "nombre de places "} ;
+		salle = new JComboBox (elements2) ;
+		salle.setBounds(10,30,220,40);
+		panel.add(salle) ;
+		JLabel label2 = new JLabel(" ") ;
+		panel.add(label2) ;
+		
+		salle.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				e.getSource();
+				String s2 = (String) salle.getSelectedItem() ;
+				label2.setText("Vous avez sélectionné " + s2 );
+				
+			}
+		});
+		
+		
+		
+		return panel ;
+		
+	}
+	
+	
+	public JComboBox getsalle() {
+		return salle ;
+	}
+	
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		Ressources ress = new Ressources() ;
+	}
+	
+    
+}
