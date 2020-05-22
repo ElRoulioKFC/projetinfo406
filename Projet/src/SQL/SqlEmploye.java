05bec21bc395760156191e1ad345b90cb9f6225e
ElRoulioKFC <56266482+ElRoulioKFC@users.noreply.github.com>
2020-05-22 14:19:56 +0200

    insertirion employe

--- Projet/src/SQL/SqlEmploye.java
+++ Projet/src/SQL/SqlEmploye.java
@@ -11,25 +11,12 @@ public class SqlEmploye extends CreationTable{
 	
 	
 	
-	public static void insertion(String nom, String prenom, String role, String motDePasse, ArrayList<String> tag, ArrayList<Integer> niveau) throws SQLException{
-		String sql = "INSERT INTO Employe(nomEmp, nom, prenom, role, motDePasse) VALUE('" + nom + "' ,  '"+prenom+"' , '"+role+"' , '"+motDePasse+"');";
+	public static void insertion(String nom, String prenom,int nbHeuresSemEmp,int nbHeureActEmp, String metier, String mailEmp, int superieurEmp, String pseudoEmp, String mdpEmp, String roleEmp, int salaireEmp  ) throws SQLException{
+		String sql = "INSERT INTO Employe(nomEmp, prenomEmp,nbHeuresSemEmp,nbHeureActEmp,metier, mailEmp, superieurEmp, pseudoEmp, mdpEmp, roleEmp, salaireEmp ) VALUE('" + nom + "' ,  '"+prenom+"' , '"+nbHeuresSemEmp + "' , '" + nbHeureActEmp+"' , '"+ metier  +"' , '"+ mailEmp  +"' , '"+ superieurEmp + "' , '" + pseudoEmp + "' , '" + mdpEmp + "' , '" + roleEmp + "' , '" + salaireEmp + "';";
 		try{
 			 Statement stmt = getCon().createStatement();
 			 stmt.executeUpdate(sql);
 			 System.out.println("insertion fait");
-		} catch(SQLException e){
-			e.printStackTrace();
-		}
-		String sql2 = "SELECT * FROM Personne ORDER BY numSalarie DESC;";
-		try{
-			 Statement stmt2 = getCon().createStatement();
-			 try (ResultSet res2 = stmt2.executeQuery(sql2)){
-				 res2.next();
-				 for (int i=0; i<tag.size(); i++){
-					 System.out.println(res2.getInt("numSalarie") + " " + tag.get(i) + " " + niveau.get(i));
-					 JavaSQLCompetence.insertion(res2.getInt("numSalarie"),tag.get(i), niveau.get(i));
-				 }
-			 }
 		} catch(SQLException e){
 			e.printStackTrace();
 		}
