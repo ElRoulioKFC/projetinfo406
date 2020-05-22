5276ffaabe3c0928d1fbb0fe7cbc7319a1e4d2dd
ElRoulioKFC <56266482+ElRoulioKFC@users.noreply.github.com>
2020-05-22 15:26:53 +0200

    up

--- Projet/src/SQL/SqlEmploye.java
+++ Projet/src/SQL/SqlEmploye.java
@@ -11,8 +11,8 @@ public class SqlEmploye extends CreationTable{
 	
 	
 	
-	public static void insertion(String nom, String prenom,int nbHeuresSemEmp,int nbHeureActEmp, String metier, String mailEmp, int superieurEmp, String pseudoEmp, String mdpEmp, String roleEmp, int salaireEmp  ) throws SQLException{
-		String sql = "INSERT INTO Employe(nomEmp, prenomEmp,nbHeuresSemEmp,nbHeureActEmp,metier, mailEmp, superieurEmp, pseudoEmp, mdpEmp, roleEmp, salaireEmp ) VALUE('" + nom + "' ,  '"+prenom+"' , '"+nbHeuresSemEmp + "' , '" + nbHeureActEmp+"' , '"+ metier  +"' , '"+ mailEmp  +"' , '"+ superieurEmp + "' , '" + pseudoEmp + "' , '" + mdpEmp + "' , '" + roleEmp + "' , '" + salaireEmp + "';";
+	public static void insertion(String nom, String prenom,int nbHeuresSemEmp,int nbHeureActEmp, String metier, String mailEmp,  String pseudoEmp, String mdpEmp, String roleEmp, int salaireEmp  ) throws SQLException{
+		String sql = "INSERT INTO Employe(nomEmp, prenomEmp,nbHeuresSemEmp,nbHeureActEmp,metier, mailEmp, pseudoEmp, mdpEmp, roleEmp, salaireEmp ) VALUE('" + nom + "' ,  '"+prenom+"' , '"+nbHeuresSemEmp + "' , '" + nbHeureActEmp+"' , '"+ metier  +"' , '"+ mailEmp  + "' , '" + pseudoEmp + "' , '" + mdpEmp + "' , '" + roleEmp + "' , '" + salaireEmp + "';";
 		try{
 			 Statement stmt = getCon().createStatement();
 			 stmt.executeUpdate(sql);
