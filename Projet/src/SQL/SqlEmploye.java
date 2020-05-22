02acc300cd0175e858b5667b75f6520b5b65eddc
ElRoulioKFC <56266482+ElRoulioKFC@users.noreply.github.com>
2020-05-22 15:58:29 +0200

    up

--- Projet/src/SQL/SqlEmploye.java
+++ Projet/src/SQL/SqlEmploye.java
@@ -5,6 +5,9 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 
+import controleur.Employe;
+import controleur.ListeEmploye;
+
 public class SqlEmploye extends CreationTable{
 
 	
@@ -23,13 +26,38 @@ public class SqlEmploye extends CreationTable{
 	}
 	
 	public static void supprime(int idEmploye) throws SQLException{
-		try{
+		try{// a rajouter les autres suppression car clef etrangere
 			 Statement stmt = getCon().createStatement();
 			 String sql = "DELETE FROM ParticipeSalarie WHERE numSalarie ="+ idEmploye;
+			 stmt.executeUpdate(sql);
+			 
+			 sql = "DELETE FROM Employe_Projet WHERE numSalarie ="+ idEmploye;
 			 stmt.executeUpdate(sql);
 			
 		} catch(SQLException e){
 			e.printStackTrace();
 		}
 	}
+	
+	public static ListeEmploye recupere() throws SQLException{
+		
+		Employe empCour = null;
+		ListeEmploye liste = new ListeEmploye();
+		String sql = " SELECT * FROM Employe ;";
+		try{
+			Statement stmt = getCon().createStatement();
+			try (ResultSet res = stmt.executeQuery(sql)){
+				 while(res.next()) {
+					empCour = new Employe(res.getInt("idEmp"),res.getString("prenomEmp"),res.getString("nomEmp"),res.getInt("salaireEmp"),res.getInt("nbHeuresSemEmp") ,res.getString("metier"),res.getString("mailEmp")) ;
+					liste.ajouter(empCour);
+				 }
+			}			 
+		} catch(SQLException e){
+			e.printStackTrace();
+		}
+				return liste;
+
+	}
+	
 }
+
